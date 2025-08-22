313a2b614454a4e786c22803f43cb6e87d217c75
Vitor <vitorhenrique_rgt@outlook.com.br>
2025-08-21 23:53:54 -0300

    🚀 feat(server): ensure server starts only after DB connection

--- api/src/index.js
+++ api/src/index.js
@@ -1,22 +1,25 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const connectDB = require('./config/database')
-const booksRouter = require('./routes/books')
+const express = require('express');
+const connectDB = require('./config/database');
+const booksRouter = require('./routes/books');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-connectDB()
+app.use(express.json());
 
-app.use(express.json)
+app.use('/api/books', booksRouter);
 
-app.use('/api/books', booksRouter)
+const startServer = async () => {
+  try {
+    await connectDB(); // Aguarda a conexão com o banco de dados
+    app.listen(port, () => {
+      console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+  } catch (err) {
+    console.error('Falha ao iniciar o servidor:', err);
+  }
+};
 
-app.get('/', (req, res) => {
-  res.send('API do App de Livros. Olá!')
-})
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`)
-})
+startServer();
