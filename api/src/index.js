b9ae888200609231b42e2c8bc6bb763fc2339118
Vitor <vitorhenrique_rgt@outlook.com.br>
2025-08-24 15:15:15 -0300

    🐛 fix(server): ensure server starts after DB connection and apply CORS

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const connectDB = require('./config/database')
 const booksRouter = require('./routes/books')
 
@@ -8,6 +9,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/books', booksRouter)
 
