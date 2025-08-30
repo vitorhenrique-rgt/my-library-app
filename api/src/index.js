db4faf104b669ea42b368bec02ab8545c27fbbb1
Vitor <vitorhenrique_rgt@outlook.com.br>
2025-08-30 14:15:57 -0300

    ✨ feat(api): Add proxy routes for Google Books API searches

--- api/src/index.js
+++ api/src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
-
 const express = require('express')
+const proxy = require('express-http-proxy')
 const cors = require('cors')
 const connectDB = require('./config/database')
 const booksRouter = require('./routes/books')
@@ -11,6 +11,25 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
+
+// Rota de proxy para detalhes de um livro específico
+app.use(
+  '/google-books/:googleBookId',
+  proxy('https://www.googleapis.com', {
+    proxyReqPathResolver: function (req) {
+      return `/books/v1/volumes/${req.params.googleBookId}?key=${process.env.GOOGLE_BOOKS_API_KEY}`
+    },
+  })
+)
+// Rota de proxy para a busca de livros
+app.use(
+  '/google-books',
+  proxy('https://www.googleapis.com', {
+    proxyReqPathResolver: function (req) {
+      return `/books/v1/volumes?q=${req.query.q}&key=${process.env.GOOGLE_BOOKS_API_KEY}`
+    },
+  })
+)
 
 app.use('/api/books', booksRouter)
 app.use('/api/auth', authRouter)
