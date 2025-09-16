6dfae3de2dd97e3830afa669a40a84b00c4764fa
Vitor <vitorhenrique_rgt@outlook.com.br>
2025-09-16 11:52:24 -0300

    ⚙  chore: Update eslint configuration

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -1,14 +1,15 @@
 // eslint.config.js
-import eslintPluginImport from "eslint-plugin-import";
-import globals from "globals";
+import eslintPluginImport from 'eslint-plugin-import'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default [
   {
-    ignores: ["node_modules", "dist", "build"],
+    ignores: ['node_modules', 'dist', 'build'],
 
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      ecmaVersion: 'latest',
+      sourceType: 'module',
       globals: {
         ...globals.node, // Ambiente Node.js
       },
@@ -20,46 +21,35 @@ export default [
 
     rules: {
       // Regras básicas
-      "no-unused-vars": "warn",
-      "no-console": "off",
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
 
       // Importações
-      "import/order": [
-        "error",
+      'import/order': [
+        'error',
         {
           groups: [
-            "builtin",
-            "external",
-            "internal",
-            "parent",
-            "sibling",
-            "index",
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
           ],
-          "newlines-between": "always",
+          'newlines-between': 'always',
         },
       ],
-      "import/no-duplicates": "error",
+      'import/no-duplicates': 'error',
 
       // Estilo de código (deixe o Prettier cuidar do resto)
       semi: false,
-      quotes: ["error", "single"],
+      quotes: ['error', 'single'],
     },
   },
 
   // Se tiver TypeScript
   {
-    files: ["**/*.ts", "**/*.tsx"],
-    languageOptions: {
-      parser: "@typescript-eslint/parser",
-      parserOptions: {
-        project: "./tsconfig.json",
-      },
-    },
-    plugins: {
-      "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-    },
-    rules: {
-      "@typescript-eslint/no-unused-vars": ["warn"],
-    },
+    files: ['**/*.ts', '**/*.tsx'],
+    extends: [...tseslint.configs.recommended, ...tseslint.configs.stylistic],
   },
-];
+]
